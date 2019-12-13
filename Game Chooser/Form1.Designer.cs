69ff0d4646d9c2a5021bdedae584419870208c27
hrjohnston <58840498+hrjohnston@users.noreply.github.com>
2019-12-13 19:44:16 +1300

    Adding all files for upload

---
+++ Game Chooser/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Game_Chooser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.lbGame = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(171, 162);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(346, 20);
+            this.tbAddress.TabIndex = 0;
+            // 
+            // lbGame
+            // 
+            this.lbGame.AutoSize = true;
+            this.lbGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGame.Location = new System.Drawing.Point(165, 35);
+            this.lbGame.Name = "lbGame";
+            this.lbGame.Size = new System.Drawing.Size(0, 36);
+            this.lbGame.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(552, 162);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click_1);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(552, 259);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(171, 259);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(140, 23);
+            this.btnNewGame.TabIndex = 5;
+            this.btnNewGame.Text = "Choose new Game?";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lbGame);
+            this.Controls.Add(this.tbAddress);
+            this.Name = "Form1";
+            this.Text = "Game Chooser 1.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label lbGame;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ Game Chooser/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Game_Chooser
+{
+    public partial class Form1 : Form
+    {
+        List<string> gamelist = new List<string>();
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void btnBrowse_Click_1(object sender, EventArgs e)
+        {
+            GetWordList();
+            GetNewWord();
+        }
+
+        public void GetWordList()
+        { 
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Choose a Text File to Load From";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = false;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                tbAddress.Text = fdlg.FileName;
+                StreamReader sr = new StreamReader(fdlg.FileName);
+                gamelist.Clear();
+                // Seed a random number in the range 0 to your count
+                var glist = File.ReadAllLines(fdlg.FileName);
+                foreach (var s in glist)
+                {
+                    gamelist.Add(s);
+                }
+            }
+        }
+
+        public void GetNewWord()
+        { 
+            int count = gamelist.Count;
+            int randomNumber = r.Next(0, count);
+            string game = gamelist[randomNumber];
+            lbGame.Text = game;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            GetNewWord();
+        }
+    }
+}
+
